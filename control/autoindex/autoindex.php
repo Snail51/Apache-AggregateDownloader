fe46d728f70f29cafac789b465dcab936f75c6f7
mobren11 <mobren11@gmail.com>
2025-02-16 16:09:51 -0600

    rearange for metadata import
    the old revision bypassed the part where the autoindex php handler was imported to do MORE than just the aggrDownloader part. this revision reverts to a solution where metadata is loaded AND the aggregate downloader is inserted

---
+++ control/autoindex/autoindex.php
@@ -0,0 +1,47 @@
+<?php
+
+	# === Error Reporting ===
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+	# === Populate Variables with Data ===
+	$dir=$_SERVER['DOCUMENT_ROOT'] . $_SERVER['REQUEST_URI'];
+	$parts=explode('/', $dir);
+	$name=array_slice($parts, -2, 1);
+	$name=end($name);
+	$name=$name;
+    
+	$request=$_SERVER['REQUEST_URI'];
+	$request=explode("?", $request)[0];
+	$request=str_replace("/host/", "/", $request);
+	$request=trim($request,"/");
+	$request="/".$request;
+	$elements=scandir(explode("?", $dir)[0]);
+	$count=count($elements)-2;
+
+	# === Start <head> ===
+	echo("\n<head>\n");
+
+	# === Replace and insert metadata ===
+	$metadata = file_get_contents("./metatags.html");
+	$metadata = str_replace("{{pageTitle}}", $request, $metadata);
+	$metadata = str_replace("{{description}}", "Browse " . $count . " files", $metadata);
+	$metadata = str_replace("{{siteName}}", "Snailien Concepts", $metadata);
+	$metadata = str_replace("{{siteURL}}", $_SERVER["SERVER_NAME"], $metadata);
+	$metadata = str_replace("{{tags}}", "index, autoindex, directory, directory listing, files, view, browse", $metadata);
+	$metadata = str_replace("{{icon}}", "/assets/image/internal/snailien_logo.jpg", $metadata);
+	echo $metadata;
+
+	# === End <head> ===
+	echo("\n</head>\n");
+
+	# === Start <body> ===
+	echo("\n<body>\n");
+	echo("<h1> Index of " . $request . "</h1>");
+
+
+	# === Insert "Download All" Script ===
+	$aggrDownload = file_get_contents("./../aggrDownload/aggrDownloadHeader.html");
+	echo $aggrDownload;
+?>
